ce883aefb3a6b70e202c22545f8c7c4c5dab9a03
root <mhdhabeb@yahoo.com>
2014-05-12 12:34:03 -0700

    update:scriptfileName >> fixed

--- py_first_lines/McAfeeNSM_URL_grep.py
+++ py_first_lines/McAfeeNSM_URL_grep.py
@@ -1,7 +1,12 @@
 import re
 import os
-from urllib2 import URLError, urlopen, Request 
+import hashlib
+import socket
+from urllib2 import URLError, HTTPError, urlopen, Request 
+from httplib import BadStatusLine
 from optparse import OptionParser
+
+#mal_analysis=load_source('wepawet_check.py', '/root/py_code/wepawet_check.py')
 
 options=OptionParser(usage='%prog [options]', description='Parse URLs from McAfeeNSM Console Raw Data')
 options.add_option("-s", "--source-file", type="string", action="store", dest="filename")
@@ -11,13 +16,14 @@ sourcefile=str(opts.filename)
 #response_check=opts.verify
 targetfile_name=(sourcefile.split('/')[-1]).split('.')[0] + "_URLs"
 targetfile=file(targetfile_name, 'w')
+#scriptfile_name=(refined_data.split('/')[-1]).strip('\n')
+
 
 def input_check():
 	if sourcefile=="None":
 		return options.print_help();
 	else:
 		grep_urls()
-		#http_response_check()
 
 def grep_urls():
 	if os.path.exists(sourcefile):
@@ -33,11 +39,13 @@ def grep_urls():
 					query=http_response_check(refined_data)
 					if "None" in str(query):
 						targetfile.write(refined_data)
-						http_fetch(refined_data)
-						#print output
+						if ".js" in refined_data:
+							http_fetch(refined_data)
+							hash_file=filename_gen(refined_data)
+							hash_gen(hash_file)
+							
 						#script_file=http_fetch(output)
-						#print script_file
-					#targetfile.write(output)
+					
 	else:
 		print "File Not Found!"
 
@@ -45,14 +53,34 @@ def http_response_check(url):
 	http_parse=Request(url)
 	try:
 	    http_open=urlopen(http_parse)
-	except URLError, http_error:
-		return http_error.reason
-	
-def http_fetch(url):
-	if ".js" in url:
-		js_read=urlopen(url).read()
-		scriptfile_name=(url.split('/')[-1]).strip('\n')
-		script_file=file(scriptfile_name, 'w')
-		return script_file.write(js_read)
+	except URLError as error:		
+		return error.reason
+	except HTTPError as  error:
+		return error.reason
+	except BadStatusLine as error:  
+		print error
+	except socket.timeout as error:
+		print error
 
+
+def filename_gen(url):
+         scriptfile_name=(url.split('/')[-1]).strip('\n')
+         return str(scriptfile_name)
+		
+def http_fetch(url):
+        scriptfile_name=(url.split('/')[-1]).strip('\n')
+	js_read=urlopen(url).read()
+	script_file=file(scriptfile_name, 'w')
+	return script_file.write(js_read)
+
+def hash_gen(inFile):
+	with open(inFile, 'r') as file:
+		sha1_hash_gen=hashlib.sha1()
+		buf=file.read(8192)
+		while len(buf)> 0:
+			sha1_hash_gen.update(buf)
+			buf=file.read(8192)
+		print sha1_hash_gen.hexdigest()
+	
+	
 input_check()
