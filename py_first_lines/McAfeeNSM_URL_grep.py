5c6a6840b5b64541e1b2850f187eb989309212cf
root <mhdhabeb@yahoo.com>
2014-05-13 05:29:58 -0700

    code structure update

--- py_first_lines/McAfeeNSM_URL_grep.py
+++ py_first_lines/McAfeeNSM_URL_grep.py
@@ -6,48 +6,49 @@ from urllib2 import URLError, HTTPError, urlopen, Request
 from httplib import BadStatusLine
 from optparse import OptionParser
 
-#mal_analysis=load_source('wepawet_check.py', '/root/py_code/wepawet_check.py')
-
-options=OptionParser(usage='%prog [options]', description='Parse URLs from McAfeeNSM Console Raw Data')
-options.add_option("-s", "--source-file", type="string", action="store", dest="filename")
+options=OptionParser(usage='%prog [options]', description='Parse URLs from McAfeeNSM Console RAWData')
+options.add_option("-s", "--source-file", type="string", action="store", dest="filename", help="locate the SourceFile to be parsed [required]")
+options.set_usage("McAfeeNSM_URL_Parser.py -s <sourcefile>")
+options.add_option("-e", "--file-extension", type="string", action="store", dest="filetype", default=".js", help="define file extension [default=%default]" )
 #options.add_option("-c", "--check-response", type="int", action="store", dest="verify",default=1)
 opts, args = options.parse_args()
 sourcefile=str(opts.filename) 
+ext=str(opts.filetype)
 #response_check=opts.verify
 targetfile_name=(sourcefile.split('/')[-1]).split('.')[0] + "_URLs"
 targetfile=file(targetfile_name, 'w')
-#scriptfile_name=(refined_data.split('/')[-1]).strip('\n')
 
 
 def input_check():
 	if sourcefile=="None":
 		return options.print_help();
 	else:
-		grep_urls()
+		main()	
 
-def grep_urls():
+def grep_urls(inDATA):
+	ip_pattern=re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+        src_ip=re.findall(ip_pattern, inDATA)
+   	xl=re.sub(r'^(.*)HTTP URI == ',"",inDATA);
+        xr=re.sub(r';(.*)',"",xl);
+	if "n/a" not in xr:
+		return "http://" + src_ip[0] + xr
+	
+def main():
 	if os.path.exists(sourcefile):
 		raw_data=open(sourcefile, 'r').readlines();
 		for line in raw_data:
 			if "HTTP URI" in line:
-				ip_pattern=re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-				src_ip=re.findall(ip_pattern, line)
-				xl=re.sub(r'^(.*)HTTP URI == ',"",line);
-	                	xr=re.sub(r';(.*)',"",xl);
-				if "n/a" not in xr:
-					refined_data="http://" + src_ip[0] + xr
-					query=http_response_check(refined_data)
-					if "None" in str(query):
-						targetfile.write(refined_data)
-						if ".js" in refined_data:
-							http_fetch(refined_data)
-							hash_file=filename_gen(refined_data)
-							hash_gen(hash_file)
-							
-						#script_file=http_fetch(output)
-					
+				refined_data=grep_urls(line)
+				targetfile.write(refined_data)
+				query=http_response_check(refined_data)
+				if "None" in str(query) and ".js" in refined_data:
+					http_fetch(refined_data)
+					hash_file=filename_gen(refined_data)
+					hash_gen(hash_file)
+												
 	else:
 		print "File Not Found!"
+
 
 def http_response_check(url):
 	http_parse=Request(url)
@@ -61,7 +62,6 @@ def http_response_check(url):
 		print error
 	except socket.timeout as error:
 		print error
-
 
 def filename_gen(url):
          scriptfile_name=(url.split('/')[-1]).strip('\n')
@@ -81,6 +81,5 @@ def hash_gen(inFile):
 			sha1_hash_gen.update(buf)
 			buf=file.read(8192)
 		print sha1_hash_gen.hexdigest()
-	
-	
+		
 input_check()
